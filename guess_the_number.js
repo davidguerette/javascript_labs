d00fc90dcc02919fc01bfe19811d26eb11857688
David Guerette <david.guerette@gmail.com>
2014-08-08 21:13:18 -0400

    Basic number guess prompt functioning

--- guess_the_number.js
+++ guess_the_number.js
@@ -1,4 +1,15 @@
-// Generate a random number
-// Get the user's name
+// Generate a random number between 1 and 10
+var target_num = Math.floor((Math.random() * 10) + 1)
+
+// alert("The answer is " + target_num)
+var name = prompt("What is your name?");
+
 // Ask the user to enter a number
+var guess = prompt("Alright " + name + ". What's your best guess?");
+
 // If the number equals the random number let the user know that they've won, other wise let them know that they lost
+if (guess == target_num) {
+  alert("That's correct! You are made of win");
+} else {
+  alert("Sorry, incorrect guess. Refresh to try again.");
+}
