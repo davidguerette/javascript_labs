d75228f0716bfb55c11bca9de487c974a57108b2
David Guerette <david.guerette@gmail.com>
2014-08-08 20:17:26 -0400

    Initial commit.

---
+++ guess_the_number.js
@@ -0,0 +1,4 @@
+// Generate a random number
+// Get the user's name
+// Ask the user to enter a number
+// If the number equals the random number let the user know that they've won, other wise let them know that they lost
